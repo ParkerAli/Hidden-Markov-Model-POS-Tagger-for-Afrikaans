bafe7ca2a48108d3499144b555b967ab3b08bfe4
ParkerAli <53168857+ParkerAli@users.noreply.github.com>
2021-05-25 23:11:22 +0200

    added CSV files and script for conversion

---
+++ AfrikaansPOSData/ChangeToCSV.R
@@ -0,0 +1,32 @@
+library(readxl)
+
+train = read_excel("GOV-ZA.50000TrainingSet.af.pos.full.xls")
+test = read_excel("GOV-ZA.5000TestSet.af.pos.full.xls")
+
+# directories changed to prevent an overwrite
+write.csv(train,"train.csv", row.names = FALSE)  
+write.csv(test,"test.csv", row.names = FALSE)
+
+
+## some quick checks
+trainCheck = read.csv("train.csv")
+testCheck = read.csv("test.csv")
+
+#check dims
+dim(trainCheck)
+dim(testCheck)
+dim(test)
+dim(train)
+View(read.csv("train.csv"))
+
+# we can see blank rows have been made NA. Might need to address these in EDA
+# NA row at every EOS
+sum(is.na(trainCheck$Token))
+sum(is.na(testCheck$Token))
+
+is.na(test)
+
+
+
+
+
