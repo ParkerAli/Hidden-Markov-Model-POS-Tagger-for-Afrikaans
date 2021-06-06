50d70d691d93a4ff6d55dccf8190d0d8a5ef66b4
ParkerAli <53168857+ParkerAli@users.noreply.github.com>
2021-06-06 23:35:33 +0200

    ALMOST FUCKING DONE

--- HMM_trigram.py
+++ HMM_trigram.py
@@ -3,9 +3,7 @@ import pandas as pd
 from sklearn import metrics
 from collections import defaultdict, Counter, deque
 from itertools import permutations, dropwhile
-import unicodedata
-
-import string
+import time
 
 import os.path
 
@@ -18,13 +16,15 @@ PUNCTUATION = {'.', ',', ';', ':', "'", '"', '$', '#', '@', '!', '?', '/', '*', 
 
 class HMM(object):
 
-    def __init__(self, dirname: str) -> None:
+    def __init__(self, dirname: str, baseline=True) -> None:
 
         self.unigram_c, self.tags, self.train,self.val, self.vocab = self.preprocess(
             pd.read_csv(f"{dirname}\\train.csv", na_filter=False), 0.7)  # I used windows so change \\ to /
         self.test = self.test_preprocess(pd.read_csv(f"{dirname}\\test.csv", na_filter=False))
+        self.baseline = baseline
 
-        
+
+    def train(self):
         self.unigram = self.unigram_probability()
         self.bigram = self.bigram_probability()
 
@@ -37,7 +37,6 @@ class HMM(object):
         else:
              with open("trigram.pkl","rb") as f:
                 self.trigram = pickle.load(f)
-
 
     def preprocess(self, df, training_proportion):
         """
@@ -53,8 +52,7 @@ class HMM(object):
         np.random.seed(69)  # set random seed for sampling
         num_sent = len(df.loc[df['Token'] == "NA"])  # number of sentences
         train_indices = np.random.choice(a=num_sent, size=int(num_sent * training_proportion), replace=False)  # list of training sents
-        print(len(train_indices))
-        print(num_sent)
+
 
         # training vars
         unigram_c = defaultdict(int)  # the unigram counts
@@ -75,29 +73,23 @@ class HMM(object):
         for key, count in dropwhile(lambda key_count: key_count[1] > 1, counter.most_common()):
             del counter[key]
 
-
         for i, row in enumerate(df.itertuples(index=False)):
-            
+
             if sent_counter in train_indices:
                 
                 if row[0] != "NA":
                     word = row[0].lower()
                     vocabulary.add(word)
                     if word in counter:
-
-                        # vocabulary.add(word)
                         unigram_c[(word, row[1])] += 1
                         train_tags[row[1]].add(i)
                         train_word_tag_pairs.append((word, row[1]))
                     else:
-                        # vocabulary.add(word)
                         unigram_c[(word, "<UNK>")] += 1
                         train_tags["<UNK>"].add(i)
                         train_word_tag_pairs.append((word, "<UNK>"))
 
                 else:
-                    # unigram_c[("</s>", "</s>")] += 1
-                    # unigram_c[("<s>", "<s>")] += 1
                     train_word_tag_pairs.append(("</s>", "</s>"))
                     train_word_tag_pairs.append(("<s>", "<s>"))
 
@@ -108,14 +100,9 @@ class HMM(object):
                 if row[0] == "NA" and row[1] == "NA":
                     sent_counter += 1
 
-
-        
         train_tags['<s>'].remove(max(train_tags['<s>']))
-        # initialises vocabulary from the word-tag pairs in the training set
-        # vocabulary = {tup[0] for tup in train_word_tag_pairs}
 
         # populates validation set
-
 
         sent_counter = 0  # reset counter for next loop
         for row in df.itertuples(index=False):
@@ -135,9 +122,10 @@ class HMM(object):
         val_word_tag_pairs.pop()
         # the last index in the dataset is NA and we need to remove unnecessary <s> token
 
-            # train_tags['<s>'].remove(max(train_tags['<s>']))
-        print(len(vocabulary))
-        print(len(val_word_tag_pairs))
+        train_tags['<s>'].remove(max(train_tags['<s>']))
+        print(f"The number of sentences in the training set is {len(train_indices)}")
+        print(num_sent)
+        print(f"The vocabulary size is {len(vocabulary)}")
 
         return unigram_c, train_tags,train_word_tag_pairs, val_word_tag_pairs, vocabulary
 
@@ -160,7 +148,7 @@ class HMM(object):
         word_tag_pairs.pop()
         return word_tag_pairs
 
-    def _emission_probability(self, word, tag) -> float:  # unigram
+    def _emission_probability(self, word, tag) -> float:
         """
         Calculates Pr(word|tag)
         :param word: a word
@@ -175,7 +163,7 @@ class HMM(object):
         return words_tag_count / tag_count
 
 
-    def bigram_counter(self, tag_1, tag_2) -> int:  # bigram numberator
+    def bigram_counter(self, tag_1, tag_2) -> int:
         """
         Calculates the bigram count for 2 tags
         :param tag_1: The first tag
@@ -210,11 +198,11 @@ class HMM(object):
         Calculates the matrix of unigram (emission) probabilities for the training data using Pr(tag) = Pr(word|tag)
         :return: unigram probability matrix
         """
-        D = defaultdict(int)
+        unigram = defaultdict(int)
 
         for word_tag in self.unigram_c:
-            D[word_tag] = self._emission_probability(*word_tag)
-        return D
+            unigram[word_tag] = self._emission_probability(*word_tag)
+        return unigram
 
     def bigram_probability(self) -> defaultdict:
         """
@@ -222,19 +210,10 @@ class HMM(object):
         :return: bigram probability matrix
         """
         bigram = defaultdict(np.float64)
-        DP_values = {}
-                
-        for tags_pair in permutations(self.tags.keys(),2):
-            # print(tags_pair)
-            if (tag := tags_pair[0]) in DP_values:
-
-                k = DP_values[tag]
-            else:
-                DP_values[tag] = k = len(self.tags[tags_pair[0]]) + len(self.vocab)
-
-            bigram[tags_pair] = (self.bigram_counter(*tags_pair)+1) / k
-
-
+        tags_list = list(self.tags.keys() - {"<\s>"})
+        for i, tag_1 in enumerate(tags_list):
+            for j, tag_2 in enumerate(tags_list):
+                bigram[(tag_1, tag_2)] = (self.bigram_counter(tag_1,tag_2)+1) / (len(self.tags[tag_1]) + len(self.vocab))
         return bigram
 
     def trigram_probability(self) -> defaultdict:
@@ -246,62 +225,63 @@ class HMM(object):
         """
         trigram = defaultdict(np.float64)
         DP_values = {}
-        # import time
-
-        # t0 = time.time()
-
         for tags_pair in permutations(self.tags.keys(),3):
 
             if (pair := (tags_pair[0], tags_pair[1])) not in DP_values:
-            #     k = DP_values[pair]
-            # else:
-
                 DP_values[pair] = self.bigram_counter(*pair) + len(self.vocab)
 
             trigram[tags_pair] = self.trigram_counter(*tags_pair) / DP_values[pair]
-
-        # t1 = time.time()
-
-        # print(t1 - t0,"seconds")
         return trigram
 
 
-    def classification(self, pairs=None,filename="test"):
+    def classification(self, pairs=None,filename="test", l1=0,l2=1):
         """
         Tags and classifies the parameterised data using the Viterbi function, writes the output to a csv file,
         and produces a classification report
         :param pairs: word tag pairs for the training data
         :param filename: name of the file to write the tagging results to
+        :param l1: weight for the unigram probability
+        :param l2: weight for the bigram probability
         :return: tagging precision
         """
         if pairs is None:
             pairs = self.test
         hits = 0
-        # offset = 0
+        tags_list = sorted(list(self.tags.keys() - {"</s>", "<s>"}))
 
+        actual_list = []
+        predicted_list = []
         with open(f"{filename}.csv","w") as output:
-            print("Token","Actual Tag","Predicted Tag",sep=",",file=output)
+            print("Token", "Actual Tag", "Predicted Tag",sep=",", file=output)
             total = 1
             loc = 0
             sentence = deque()
-            c=0
+            c = 0
             for word,tag in pairs:
                 sentence.append((word,tag))
                 if tag == "</s>":
                     sentence.popleft()
                     sentence.pop()
-                    for actual,predicted in zip(sentence,self.dp_viterbi_algorithm(sentence)):
-                        print(actual[0],actual[1],predicted[1],sep=",",file=output)
-                        if actual[1] == predicted[1]:
-                            loc += 1
-                        if predicted[1][0] == predicted[1][0]:
-                            c +=1
+                    if self.baseline:
+                        for actual, predicted in zip(sentence, self.dp_viterbi_algorithm_bigram(sentence, tags_list)):
+                            print(actual[0],actual[1],predicted[1],sep=",",file=output)
+                            if actual[1] == predicted[1]:
+                                loc += 1
+                            if predicted[1][0] == predicted[1][0]:
+                                c +=1
+                    else:
+                        for actual,predicted in zip(sentence,self.dp_viterbi_algorithm(sentence, l1,l2, tags_list)):
+                            print(actual[0], actual[1], predicted[1], sep=",", file=output)
+                            if actual[1] == predicted[1]:
+                                loc += 1
+                            if predicted[1][0] == predicted[1][0]:
+                                c += 1
 
                     # print("\nPRECISION:",loc/len(sentence))
                     total += len(sentence)
                     hits += loc
                     loc = 0
-                    sentence = deque()
+                    sentence.clear()
 
 
         print(c/total)
@@ -324,26 +304,43 @@ class HMM(object):
         :return: linear interpolation of the unigram, bigram and trigram probabilities
         """
         l3 = 1-l1-l2
-
-        # print(word,tag3,self.unigram[(word,tag3)])
         unigram_p = self.unigram[(word,tag3)]  # unigram prediction
         bigram_p = self.bigram[(tag2, tag3)]  # bigram prediction
         trigram_p = self.trigram[(tag1, tag2, tag3)]  # trigram prediction
         return l1*unigram_p + l2*bigram_p + l3*trigram_p
 
-    def dp_viterbi_algorithm(self, sentence):
+
+    def interpolation_gridsearch(self):
+        """
+        Iterates the classification function over a grid of parameter values for lambda_1 and lambda_2 and returns the
+        values which maximise classification accuracy
+        :return: optimal values for lambda_1 and lambda_2
+        """
+        # parameter grid
+        l1_grid = np.arange(0, 1.5, 0.5)
+        l2_grid = np.arange(0, 1.5, 0.5)
+        l1_max = l2_max = cmax = 0
+
+        for i in l1_grid:
+            for j in l2_grid:
+                if i+j < 1:
+                    current = self.classification(self.test, l1=i, l2=j)
+                    if current > cmax:
+                        l1_max = i
+                        l2_max = j
+                        cmax = current
+                        print("Max Precision = ",cmax, "l1 = ", l1_max, "l2 = ", l2_max)
+        return l1_max, l2_max
+
+    def dp_viterbi_algorithm(self, sentence, l1,l2, tags_list):
         """
         Produces a tag prediction for a word by implementing the Viterbi algorithm using the linearly interpolated
         probabilities from the interpolation() function.
         :param sentence: sentence to be tagged
         :return: words and their corresponding tags
         """
-        if not sentence:
-            Exception("Sentence not found.")
-        tags_list = self.tags.keys() #- set(["</s>"])
-
-        l1 = 0
-        l2 = 0.6
+        l1 = l1
+        l2 = l2
 
         p_max_tag = "<UNK>"
         for i,(word,_) in enumerate(sentence):
@@ -351,98 +348,55 @@ class HMM(object):
             p_max = float('-inf')
             if word not in self.vocab:
                 p_max_tag = "<UNK>"
-            for tag_1,tag_2 in permutations(tags_list, 2):
-
-                if i > 1:
-                    tag_prob = self.interpolation(word,l1,l2,p_max_tag, tag_1,tag_2)
-                elif i == 1:
-                    tag_prob = self.interpolation(word,l1,l2,"<s>", tag_1,tag_2)
-                elif i == 0:
-                    tag_prob = self.interpolation(word,l1,l2,"</s>", "<s>",tag_1)
-
-
-                emission_probability = self.unigram[(word, tag_1)]
-                state_prob = emission_probability * tag_prob
-                if p_max < state_prob:
-                    p_max_tag = tag_1
-                    p_max = state_prob
-
-            yield word, p_max_tag
-
-
-    def dp_viterbi_algorithm_bigram(self, transition_matrix, sentence=None):
-        """
-        Produces a tag prediction by implementing the Viterbi algorithm using the bigram probabilities
-        :param transition_matrix: the matrix of bigram probabilities
-        :param sentence: sentence to be tagged
-        :return: words and their corresponding tags
-        """
-        if not sentence:
-            Exception("Sentence not found.")
-
-        tags_list = self.tags.keys() - set(["</s>"])
-
-        p_max_tag = "<s>"
-        for i,(word,_) in enumerate(sentence):
-
-            p_max = float('-inf')
-            if word not in self.vocab:
-                
-                for tag in tags_list:
-                    
-                    p_max_running = self.bigram[(tag,p_max_tag)]#self.interpolation(l1,l2,tag,prior_tags[-1] ,prior_tags[-2])
-                    if p_max_running> p_max:
-                        p_max_tag = tag
-                        p_max = p_max_running 
-
-                yield word, "<UNK>"
             else:
-                for tag_1 in tags_list:
+                for tag_1,tag_2 in permutations(tags_list, 2):
+                    if i > 1:
+                        tag_prob = self.interpolation(word,l1,l2,p_max_tag, tag_1,tag_2)
+                    elif i == 1:
+                        tag_prob = self.interpolation(word,l1,l2,"<s>", tag_1,tag_2)
+                    elif i == 0:
+                        tag_prob = self.interpolation(word,l1,l2,"</s>", "<s>",tag_1)
 
-                    tag_prob = transition_matrix[(p_max_tag, tag_1)]
                     emission_probability = self.unigram[(word, tag_1)]
                     state_prob = emission_probability * tag_prob
                     if p_max < state_prob:
                         p_max_tag = tag_1
                         p_max = state_prob
-            
-                yield word, p_max_tag
 
+            yield word, p_max_tag
+
+    def dp_viterbi_algorithm_bigram(self, words, tags_list):
+
+        for i, (word,_) in enumerate(words):
+            if word not in self.vocab:
+                p_max_tag = "<UNK>"
+
+            else:
+                p_max = float('-inf')
+                p_max_tag = "<s>"
+                for j, tag in enumerate(tags_list):
+                    if i == 0:
+                        tag_prob = self.bigram[("<s>", tag)]
+                    else:
+                        tag_prob = self.bigram[(p_max_tag, tag)]
+                    emission_probability = self.unigram[(word, tag)]
+                    state_prob = emission_probability * tag_prob
+
+                    if p_max < state_prob:
+                        p_max_tag = tag
+                        p_max = state_prob
+            yield word, p_max_tag
 
 
 if __name__ == "__main__":
-    hmm = HMM("AfrikaansPOSData")
-
-    sentence = [
-        ("laai", "VTHOG"),
-        ("die", "LB"),
-        ("elektroniese", "ASA"),
-        ("aansoekvorm", "NSE"),
-        ("af", "UPW"),
-        ("of", "KN"),
-        ("kry", "VTHOG"),
-        ("dit", "PDOENP"),
-        ("by", "SVS"),
-        ("jou", "PTEB"),
-        ("naaste", "AOA"),
-        ("said-kantoor", "NSE"),
-        (".", "ZE")
-    ]
-
-    import time
-
     t0 = time.time()
-    print(hmm.classification(hmm.train,"training"))
+    hmm = HMM("AfrikaansPOSData", baseline=False)
+    print(f"Data Processing and Training:")
+    print(hmm.classification(hmm.val,"test"))
 
+    # if hmm.baseline:
+    #     hmm.interpolation_gridsearch()
+    #     print(hmm.classification(hmm.val, "test", hmm.interpolation_gridsearch()))
     print(f"Training runtime = {time.time()-t0} seconds")
 
-    # t0 = time.time()
-
-    # print(hmm.classification(hmm.test,"testing"))
-    # print(f"Testing runtime = {time.time()-t0} seconds")
-
-    # t0 = time.time()
-
-    # print(hmm.classification(hmm.val,"validation"))
-    # print(f"Validation runtime = {time.time()-t0} seconds")
 
